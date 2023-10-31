66f9fe7f158ff1a6aae279ea6d24556d85e2e71d
userNameBHL <127075672+userNameBHL@users.noreply.github.com>
2023-11-01 03:08:55 +0900

    LBH
    LBH

---
+++ my_project/backend/fitdb_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FitdbAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fitdb_app"
--- my_project/backend/fitdb_project/asgi.py
+++ my_project/backend/fitdb_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fitdb project.
+ASGI config for fitdb_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitdb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitdb_project.settings")
 
 application = get_asgi_application()
